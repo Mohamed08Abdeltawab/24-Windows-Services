7abdcdeb77a9b87f4dba4d909b50f831e33a0bd1
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-16 18:04:34 +0300

    Module2 -> Debugging a Windows Service Using Console Mode

---
+++ Code/Module2- MyFullServiceStateImplementation/Module2Test- Debugging wih Console Mode/Module2Test.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Test
+{
+    public partial class Module2Test : ServiceBase
+    {
+        private string logDirectory;
+        private string logFilePath;
+        public Module2Test()
+        {
+            InitializeComponent();
+            CanPauseAndContinue = true;
+            CanShutdown = true;
+
+            logDirectory = ConfigurationManager.AppSettings["LogDirectory"];
+
+            if (string.IsNullOrWhiteSpace(logDirectory))
+            {
+                throw new ConfigurationErrorsException("LogDirectory is not configured in the application settings.");
+            }
+
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+            logFilePath = Path.Combine(logDirectory, "ServiceStateLog.txt");
+        }
+
+
+
+
+        public void EventLogWrite(string message)
+        {
+            string messageToWrite = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}\n";
+            File.AppendAllText(logFilePath, messageToWrite);
+
+            //for debugging 
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine(messageToWrite);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLogWrite("Service started.");
+        }
+
+        protected override void OnStop()
+        {
+            EventLogWrite("Service stopped.");
+        }
+
+        protected override void OnPause()
+        {
+            EventLogWrite("Service paused.");
+        }
+
+        protected override void OnContinue()
+        {
+            EventLogWrite("Service continued.");
+        }
+
+        protected override void OnShutdown()
+        {
+            EventLogWrite("Service shutdown.");
+        }
+
+        public void StartInConsole()
+        {
+            OnStart(null);
+            Console.WriteLine("Service started. Press any key to stop...");
+            Console.ReadLine();
+            OnStop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Code/Module2- MyFullServiceStateImplementation/Module2Test- Debugging wih Console Mode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Test
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //if you in debug mode, you can run the service as a console application
+            //if you in release mode, you can run the service as a windows service return false and run the service as a windows service
+            if (Environment.UserInteractive)
+            {
+                Module2Test service = new Module2Test();
+                service.StartInConsole();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new Module2Test()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
