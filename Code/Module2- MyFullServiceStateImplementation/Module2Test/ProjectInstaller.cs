a88194dd4c6e8c51c1e6ec5cbe4e53d17df14b3a
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-16 17:35:35 +0300

    Module2 -> Full Implementation for Service States and Events and creating my own servicess

---
+++ Code/Module2- MyFullServiceStateImplementation/Module2Test/ProjectInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace Module2Test
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalService
+            };
+
+            serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "Module2Test",
+                DisplayName = "Module2 Test",
+                Description = "Creating Services to testing the event of start and stop and pause and resume",
+                StartType = ServiceStartMode.Automatic
+
+            };
+
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+
+    }
+}
