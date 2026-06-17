fa7182b3f7bafd763770b8fb617e59bd9ebbebb5
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-17 22:47:05 +0300

    Module4 -> Configuring Service Dependencies

---
+++ Code/Module4- Dependencies Service/DependenciesService/DependenciesServiceInstall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace DependenciesService
+{
+    [RunInstaller(true)]
+    public partial class DependenciesServiceInstall : Installer
+    {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+        public DependenciesServiceInstall()
+        {
+            processInstaller = new ServiceProcessInstaller()
+            {
+                Account = ServiceAccount.LocalSystem
+            };
+
+            serviceInstaller = new ServiceInstaller()
+            {
+                ServiceName = "DependenciesService",
+                DisplayName = "Dependencies Service",
+                Description = "A service that demonstrates dependencies.",
+                ServicesDependedOn = new string[] { "EventLog", "RpcSs" },
+                StartType = ServiceStartMode.Automatic
+            };
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
