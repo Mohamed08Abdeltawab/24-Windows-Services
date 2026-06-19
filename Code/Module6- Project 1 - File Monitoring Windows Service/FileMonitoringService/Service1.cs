1c1e5a5d2fd3a5066ee88ccb0b42a1f2e9b9da5e
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-19 22:53:56 +0300

    Module6 -> FileMonitoringService -> made app.config & using FileSystemWatcher

---
+++ Code/Module6- Project 1 - File Monitoring Windows Service/FileMonitoringService/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace FileMonitoringService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        private FileSystemWatcher _watcher;
+        //get value of pathes
+        string sourcePath = ConfigurationManager.AppSettings["SourceFolder"];
+        string destinationPath = ConfigurationManager.AppSettings["DestinationFolder"];
+        string logFilePath = ConfigurationManager.AppSettings["LogPath"];
+
+
+        protected override void OnStart(string[] args)
+        {
+            //soruce path is file watcher
+            _watcher = new FileSystemWatcher(sourcePath);
+
+            //subscrib in event OnCreate
+
+            //enable raising event = true
+        }
+
+        protected override void OnStop()
+        {
+            //disable raising event = false
+
+            //dispose object
+        }
+    }
+}
